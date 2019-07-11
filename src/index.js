67b12763406dce414e2767e730ebcbdcbe17574b
Mason Ellwood <mason@fyresite.com>
2019-07-11 11:50:20 -0700

    added conditional render meathod and state direct assignment in constructor

--- src/index.js
+++ src/index.js
@@ -2,13 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-    render() {
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
-            (err) => console.log(err), 
-        );
+    constructor(props) {
+        super(props);
 
-        return <div>Latitude: </div>
+        // this is the only time we do direct assignment to this.state
+        this.state = { lat: null, errorMessage: '' };
+
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                this.setState({ lat: position.coords.latitude });
+                // have to call setState
+                // we did not use direct assignment!
+            },
+            err => {
+                this.setState({ errorMessage: err.message });
+            }
+        );
+    }
+
+    // have to define render
+    render() {
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>
+        } 
+
+        if (!this.state.errorMessage && this.state.lat) {
+            return <div>Latitude: {this.state.lat}</div>
+        }
+
+        return <div>Loading!</div>
     } 
 }
 
